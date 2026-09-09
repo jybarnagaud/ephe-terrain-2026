051ceffa0879cd783d176a27f9029db385107e57
jybarnagaud <jeanyves.barnagaud@gmail.com>
2026-09-09 14:15:46 +0200

    initial commit

--- scripts/ephe-terrain-2026-analyses.R
+++
@@ -1,9 +0,0 @@
-#***************************************************#
-# ----         UE terrain 2026            ---- #
-
-# Master BE - Ecole Pratique des Hautes Etudes #
-
-# Groupe écoacoustique des paysages du Vercors #
-# Analyse des données 
-# Contact : Killian Rey (killian.rey@etu.ephe.psl.eu)
-#***************************************************#
